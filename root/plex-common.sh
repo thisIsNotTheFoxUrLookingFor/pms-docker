80279613ccba7c17b858c1e17f0d10c28af6e6f3
tortxoFFoxtrot <155745095+tortxoFFoxtrot@users.noreply.github.com>
2024-08-13 12:23:45 +1000

    fix local url

--- root/plex-common.sh
+++ root/plex-common.sh
@@ -45,7 +45,7 @@ function getVersionInfo {
   readVarFromConf "plex_build" plexBuild
   readVarFromConf "plex_distro" plexDistro
 
-  local url="https://artifacts.plex.tv/plex-media-server-experimental/1.40.6.8838-0f37280c7/debian/plexmediaserver_1.40.6.8838-0f37280c7_amd64.deb"
+  local url="https://artifacts.plex.tv/plex-media-server-experimental/1.40.6.8849-a0fe53a79/debian/plexmediaserver_1.40.6.8849-a0fe53a79_amd64.deb"
   wget https://artifacts.plex.tv/plex-media-server-experimental/1.40.6.8849-a0fe53a79/debian/plexmediaserver_1.40.6.8849-a0fe53a79_amd64.deb
   dpkg -i plexmediaserver_1.40.6.8838-0f37280c7_amd64.deb
 
